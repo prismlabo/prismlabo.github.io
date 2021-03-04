57d6013229f2499f0b6515f62354a6e9ed84708f
TEAM PRISMLABO <74401648+prismlabo@users.noreply.github.com>
2021-03-04 19:17:09 +0900

    Add files via upload

---
+++ Special Mad Edition/mad-edition/Uranium-Barrel-3D/webgl_gltf_loader/examples/textures/equirectangular/index.js
@@ -0,0 +1,20 @@
+export const environments = [
+    {
+        id: '',
+        name: 'None',
+        path: null,
+        format: '.hdr'
+  },
+    {
+        id: 'venice-sunset',
+        name: 'Venice Sunset',
+        path: 'examples/textures/equirectangular/OvercloudHDRI.hdr',
+        format: '.hdr'
+  },
+    {
+        id: 'footprint-court',
+        name: 'Footprint Court (HDR Labs)',
+        path: 'examples/textures/equirectangular/footprint_court_2k.hdr',
+        format: '.hdr'
+  }
+];
